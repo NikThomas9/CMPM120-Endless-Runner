73e7ce5805a82b2ce83c334847b8f3c2a908bd2e
ssuvarnaTech <ssuvarna@ucsc.edu>
2021-04-25 23:21:59 -0700

    fixed HEAD changes

--- src/main.js
+++ src/main.js
@@ -13,10 +13,6 @@ let config = {
         }
     }
 };
-<<<<<<< HEAD
-=======
-
->>>>>>> a97da50564c93e95745ced56593a069e0b73962d
 
 let game = new Phaser.Game(config);
 
@@ -25,11 +21,7 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-<<<<<<< HEAD
 let keyUP, keyR, keyS;
-=======
-let keyUP, keyR;
->>>>>>> a97da50564c93e95745ced56593a069e0b73962d
 
 let score = 0;
 let highScore = 0;
